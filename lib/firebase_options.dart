e88fa520a87f0ee847c6a400ba257d0f12cb3b98
İlke Yücel <yucel.ilke@gmail.com>
2024-03-14 18:32:42 +0200

    Add Firebase config library

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,7 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -54,11 +54,11 @@ class DefaultFirebaseOptions {
   }
 
   static final FirebaseOptions ios = FirebaseOptions(
-   apiKey: dotenv.env['API_KEY']!,
-   appId: dotenv.env['APP_ID']!,
-   messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
-   projectId: dotenv.env['PROJECT_ID']!,
-   storageBucket: dotenv.env['STORAGE_BUCKET']!,
-   iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
+    apiKey: FlutterConfig.get('API_KEY'),
+    appId: FlutterConfig.get('APP_ID'),
+    messagingSenderId: FlutterConfig.get('MESSAGING_SENDER_ID'),
+    projectId: FlutterConfig.get('PROJECT_ID'),
+    storageBucket: FlutterConfig.get('STORAGE_BUCKET'),
+    iosBundleId: FlutterConfig.get('IOS_BUNDLE_ID'),
   );
 }
